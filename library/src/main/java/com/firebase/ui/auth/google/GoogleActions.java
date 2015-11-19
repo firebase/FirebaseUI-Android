17c2f9b68a65b330468acddd3b7763843f130916
Abe Haskins <abeisgreat@abeisgreat.com>
2015-11-18 16:02:04 -0800

    Adds new action files

---
+++ library/src/main/java/com/firebase/ui/auth/google/GoogleActions.java
@@ -0,0 +1,9 @@
+package com.firebase.ui.auth.google;
+
+public class GoogleActions {
+    private static int base = 1000;
+    public static int REQUEST = base+0;
+    public static int SUCCESS = base+1;
+    public static int PROVIDER_ERROR = base+2;
+    public static int USER_ERROR = base+3;
+}
