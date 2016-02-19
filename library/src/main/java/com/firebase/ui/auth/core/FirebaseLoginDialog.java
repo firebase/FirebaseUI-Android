a95111b95c97159a87ca268f151fe459efb9a762
Goddchen <goddchen@googlemail.com>
2016-02-19 19:16:35 +0100

    use support-v7 AlertDialog instead of the native one

--- library/src/main/java/com/firebase/ui/auth/core/FirebaseLoginDialog.java
+++ library/src/main/java/com/firebase/ui/auth/core/FirebaseLoginDialog.java
@@ -1,11 +1,12 @@
 package com.firebase.ui.auth.core;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -55,6 +56,7 @@ public class FirebaseLoginDialog extends DialogFragment {
         }
     }
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
