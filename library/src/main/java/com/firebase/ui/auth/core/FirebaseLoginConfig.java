d9210807fd6033c6ab0130c3bcd9275433f29a6d
Uday <uday.bidkar@gmail.com>
2016-02-20 10:29:12 -0800

    Throw IllegalArgumentException if Facebook permissions are set without enabling provider

--- library/src/main/java/com/firebase/ui/auth/core/FirebaseLoginConfig.java
+++ library/src/main/java/com/firebase/ui/auth/core/FirebaseLoginConfig.java
@@ -1,6 +1,7 @@
 package com.firebase.ui.auth.core;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,12 +18,16 @@ public class FirebaseLoginConfig {
                                 boolean isGoogleProviderEnabled,
                                 boolean isFacebookProviderEnabled,
                                 boolean isTwitterProviderEnabled,
-                                List<String> facebookPermissions){
+                                @Nullable List<String> facebookPermissions){
         this.isPasswordProviderEnabled = isPasswordProviderEnabled;
         this.isGoogleProviderEnabled = isGoogleProviderEnabled;
         this.isFacebookProviderEnabled = isFacebookProviderEnabled;
         this.isTwitterProviderEnabled = isTwitterProviderEnabled;
-        this.facebookPermissions = facebookPermissions;
+        if (facebookPermissions == null) {
+            this.facebookPermissions = new ArrayList<>(Arrays.asList("public_profile"));
+        } else {
+            this.facebookPermissions = facebookPermissions;
+        }
     }
 
     public static class Builder {
@@ -30,21 +35,10 @@ public class FirebaseLoginConfig {
         private boolean mIsGoogleProviderEnabled;
         private boolean mIsFacebookProviderEnabled;
         private boolean mIsTwitterProviderEnabled;
-
-        public Builder setFacebookPermissions(@NonNull List<String> mFacebookPermissions) {
-            this.mFacebookPermissions = mFacebookPermissions;
-            return this;
-        }
-
-        public List<String> mFacebookPermissions = new ArrayList<>(Arrays.asList("public_profile"));
+        public List<String> mFacebookPermissions;
 
         public Builder setGoogleProviderEnabled(boolean isGoogleProviderEnabled) {
             this.mIsGoogleProviderEnabled = isGoogleProviderEnabled;
-            return this;
-        }
-
-        public Builder setFacebookProviderEnabled(boolean isFacebookProviderEnabled) {
-            this.mIsFacebookProviderEnabled = isFacebookProviderEnabled;
             return this;
         }
 
@@ -58,7 +52,20 @@ public class FirebaseLoginConfig {
             return this;
         }
 
+        public Builder setFacebookProviderEnabled(boolean isFacebookProviderEnabled) {
+            this.mIsFacebookProviderEnabled = isFacebookProviderEnabled;
+            return this;
+        }
+
+        public Builder setFacebookPermissions(@NonNull List<String> mFacebookPermissions) {
+            this.mFacebookPermissions = mFacebookPermissions;
+            return this;
+        }
+
         public FirebaseLoginConfig build() {
+            if (mFacebookPermissions != null && !mIsFacebookProviderEnabled) {
+                throw new IllegalArgumentException("Facebook provider needs to be enabled if Facebook permissions are set.");
+            }
             return new FirebaseLoginConfig(mIsPasswordProviderEnabled, mIsGoogleProviderEnabled,
                     mIsFacebookProviderEnabled, mIsTwitterProviderEnabled, mFacebookPermissions);
         }
