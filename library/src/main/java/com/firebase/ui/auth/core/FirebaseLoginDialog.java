f69cc42619ee81c22d47516d5b821161ab82e864
Uday <uday.bidkar@gmail.com>
2016-02-20 10:26:56 -0800

    Add getters for other auth providers as well

--- library/src/main/java/com/firebase/ui/auth/core/FirebaseLoginDialog.java
+++ library/src/main/java/com/firebase/ui/auth/core/FirebaseLoginDialog.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -16,6 +17,8 @@ import com.firebase.client.Firebase;
 import com.firebase.ui.R;
 import com.firebase.ui.auth.facebook.FacebookAuthProvider;
 import com.firebase.ui.auth.google.GoogleAuthProvider;
+import com.firebase.ui.auth.password.PasswordAuthProvider;
+import com.firebase.ui.auth.twitter.TwitterAuthProvider;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -142,7 +145,10 @@ public class FirebaseLoginDialog extends DialogFragment {
         }
         if (config.isFacebookProviderEnabled) {
             setEnabledProvider(AuthProviderType.FACEBOOK);
-            getFacebookAuthProvider().setPermissions(config.facebookPermissions);
+            FacebookAuthProvider facebookAuthProvider = getFacebookAuthProvider();
+            if (facebookAuthProvider != null) {
+                getFacebookAuthProvider().setPermissions(config.facebookPermissions);
+            }
         }
         if (config.isGoogleProviderEnabled) {
             setEnabledProvider(AuthProviderType.GOOGLE);
@@ -172,11 +178,23 @@ public class FirebaseLoginDialog extends DialogFragment {
         });
     }
 
-    private GoogleAuthProvider getGoogleAuthProvider() {
+    @Nullable
+    public GoogleAuthProvider getGoogleAuthProvider() {
         return (GoogleAuthProvider) mEnabledProvidersByType.get(AuthProviderType.GOOGLE);
     }
 
-    private FacebookAuthProvider getFacebookAuthProvider() {
+    @Nullable
+    public FacebookAuthProvider getFacebookAuthProvider() {
         return (FacebookAuthProvider) mEnabledProvidersByType.get(AuthProviderType.FACEBOOK);
+    }
+
+    @Nullable
+    public TwitterAuthProvider getTwitterAuthProvider() {
+        return (TwitterAuthProvider) mEnabledProvidersByType.get(AuthProviderType.TWITTER);
+    }
+
+    @Nullable
+    public PasswordAuthProvider getPasswordAuthProvider() {
+        return (PasswordAuthProvider) mEnabledProvidersByType.get(AuthProviderType.PASSWORD);
     }
 }
