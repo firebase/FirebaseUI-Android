8e1b60844c78e0b7cba0cb5502db244fe11784c5
Abe Haskins <abeisgreat@abeisgreat.com>
2016-03-25 14:32:49 -0700

    Empty com.firebase.ui.auth

--- library/src/main/java/com/firebase/ui/auth/google/GoogleActions.java
+++
@@ -1,7 +0,0 @@
-package com.firebase.ui.auth.google;
-
-public class GoogleActions {
-    private static int base = 9000;
-    public static int SIGN_IN = base+0;
-    public static int SIGN_OUT = base+1;
-}
