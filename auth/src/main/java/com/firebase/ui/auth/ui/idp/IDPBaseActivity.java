3f0e51df4882e20e46abbae1a44ab2a1d6ec2195
Sam Stern <samstern@google.com>
2016-05-03 15:19:34 -0700

    Centralzing gradle configurations for submodules
    Bug: 28524186
    Change-Id: I52f618ef5335d402db783f43d0cfb1f86b246acd

--- auth/src/main/java/com/firebase/ui/auth/ui/idp/IDPBaseActivity.java
+++ auth/src/main/java/com/firebase/ui/auth/ui/idp/IDPBaseActivity.java
@@ -1,5 +1,6 @@
 package com.firebase.ui.auth.ui.idp;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.firebase.ui.auth.choreographer.Controller;
@@ -8,8 +9,8 @@ import com.firebase.ui.auth.ui.BaseActivity;
 
 public class IDPBaseActivity extends BaseActivity {
 
-    public static final int EMAIL_LOGIN_NEEDED = RESULT_FIRST_USER + 2;
-    public static final int LOGIN_CANCELLED = RESULT_FIRST_USER + 3;
+    public static final int EMAIL_LOGIN_NEEDED = Activity.RESULT_FIRST_USER + 2;
+    public static final int LOGIN_CANCELLED = Activity.RESULT_FIRST_USER + 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
