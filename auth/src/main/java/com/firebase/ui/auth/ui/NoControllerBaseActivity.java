dceeab4c254c71cd9009db99b9cac51b503bb904
Aaron Mandle <amandle@google.com>
2016-05-10 13:28:27 -0700

    Remove unnecessary CredentialsInit activity
    Change-Id: I68ddc35e9bdeb2f4c3ff594bbb421d68ea6e1162

--- auth/src/main/java/com/firebase/ui/auth/ui/NoControllerBaseActivity.java
+++ auth/src/main/java/com/firebase/ui/auth/ui/NoControllerBaseActivity.java
@@ -3,19 +3,24 @@ package com.firebase.ui.auth.ui;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.firebase.ui.auth.AuthFlowFactory;
 import com.firebase.ui.auth.choreographer.ControllerConstants;
 import com.firebase.ui.auth.choreographer.idp.provider.IDPProviderParcel;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
 public abstract class NoControllerBaseActivity extends android.support.v7.app.AppCompatActivity {
+    private static final String TAG = "NoControllerBaseActivity";
     protected String mAppName;
     protected ArrayList<IDPProviderParcel> mProviderParcels;
     protected String mTermsOfServiceUrl;
+    protected int mTheme;
 
     // TODO once the controller-centric BaseActivity is gone this will no longer be duplicate code
     private ProgressDialog mProgressDialog;
@@ -39,11 +44,15 @@ public abstract class NoControllerBaseActivity extends android.support.v7.app.Ap
                 ControllerConstants.EXTRA_PROVIDERS);
         mTermsOfServiceUrl = getIntent().getStringExtra(
                 ControllerConstants.EXTRA_TERMS_OF_SERVICE_URL);
+        mTheme = getIntent().getIntExtra(
+                ControllerConstants.EXTRA_THEME,
+                AuthFlowFactory.DEFAULT_THEME);
     }
 
     private Intent addExtras(Intent intent) {
         intent.putExtra(ControllerConstants.EXTRA_APP_NAME, mAppName)
                 .putExtra(ControllerConstants.EXTRA_TERMS_OF_SERVICE_URL, mTermsOfServiceUrl)
+                .putExtra(ControllerConstants.EXTRA_THEME, mTheme)
                 .putExtra(ControllerConstants.EXTRA_PROVIDERS, mProviderParcels);
         return intent;
     }
@@ -60,6 +69,20 @@ public abstract class NoControllerBaseActivity extends android.support.v7.app.Ap
 
     protected FirebaseApp getFirebaseApp() {
          return FirebaseApp.getInstance(mAppName);
+    }
+
+    protected FirebaseApp getFirebaseApp(String appname, String apiaryKey, String applicationId) {
+        try{
+            return FirebaseApp.getInstance(mAppName);
+        } catch (IllegalStateException e) {
+            Log.d(TAG, "FirebaseApp is not created yet, creating...");
+            FirebaseOptions options
+                    = new FirebaseOptions.Builder()
+                    .setApiKey(apiaryKey)
+                    .setApplicationId(applicationId)
+                    .build();
+            return FirebaseApp.initializeApp(this, options, mAppName);
+        }
     }
 
     protected FirebaseAuth getFirebaseAuth() {
