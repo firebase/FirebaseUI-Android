0f34c409419d17b7fed95da0470ed2cd15713f04
Alex Saveau <SUPERCILEX@users.noreply.github.com>
2016-09-01 16:11:30 -0700

    Update FirebaseListAdapter.java

--- database/src/main/java/com/firebase/ui/database/FirebaseListAdapter.java
+++ database/src/main/java/com/firebase/ui/database/FirebaseListAdapter.java
@@ -15,6 +15,7 @@
 package com.firebase.ui.database;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
