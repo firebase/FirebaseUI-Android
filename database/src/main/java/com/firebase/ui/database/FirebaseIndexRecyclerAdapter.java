9d8f7f6851d942529decab35bbc98bd59b43b787
Alex Saveau <gojunkmailx@gmail.com>
2016-10-27 20:31:06 -0700

    Remove unnecessary constructor

--- database/src/main/java/com/firebase/ui/database/FirebaseIndexRecyclerAdapter.java
+++ database/src/main/java/com/firebase/ui/database/FirebaseIndexRecyclerAdapter.java
@@ -16,7 +16,6 @@ package com.firebase.ui.database;
 
 import android.support.v7.widget.RecyclerView;
 
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 
 /**
@@ -74,26 +73,7 @@ public abstract class FirebaseIndexRecyclerAdapter<T, VH extends RecyclerView.Vi
                                         int modelLayout,
                                         Class<VH> viewHolderClass,
                                         Query keyRef,
-                                        DatabaseReference dataRef) {
-        super(modelClass, modelLayout, viewHolderClass, new FirebaseIndexArray(keyRef, dataRef));
-    }
-
-    /**
-     * @param modelClass      Firebase will marshall the data at a location into an instance of a class that you provide
-     * @param modelLayout     This is the layout used to represent a single item in the list. You will be responsible for populating an
-     *                        instance of the corresponding view with the data from an instance of modelClass.
-     * @param viewHolderClass The class that hold references to all sub-views in an instance modelLayout.
-     * @param keyRef          The Firebase location containing the list of keys to be found in {@code dataRef}.
-     *                        Can also be a slice of a location, using some
-     *                        combination of {@code limit()}, {@code startAt()}, and {@code endAt()}.
-     * @param dataRef         The Firebase location to watch for data changes.
-     *                        Each key key found at {@code keyRef}'s location represents a list item in the {@code RecyclerView}.
-     */
-    public FirebaseIndexRecyclerAdapter(Class<T> modelClass,
-                                        int modelLayout,
-                                        Class<VH> viewHolderClass,
-                                        DatabaseReference keyRef,
-                                        DatabaseReference dataRef) {
+                                        Query dataRef) {
         super(modelClass, modelLayout, viewHolderClass, new FirebaseIndexArray(keyRef, dataRef));
     }
 }
