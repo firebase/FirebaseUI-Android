da54dee7ec4f8517feff7de6e16ecfba7d903e65
Alex Saveau <gojunkmailx@gmail.com>
2016-11-29 11:33:29 -0800

    Add backwards compatibility

---
+++ auth/src/main/java/com/firebase/ui/auth/ui/ResultCodes.java
@@ -0,0 +1,19 @@
+package com.firebase.ui.auth.ui;
+
+import com.firebase.ui.auth.AuthUI;
+
+/**
+ * Result codes returned when using {@link AuthUI.SignInIntentBuilder#build()} with
+ * {@code startActivityForResult}.
+ */
+public class ResultCodes {
+
+    /**
+     * Sign in failed due to lack of network connection
+     *
+     * @deprecated Please use {@link com.firebase.ui.auth.ResultCodes#NO_NETWORK}
+     **/
+    @Deprecated
+    public static final int RESULT_NO_NETWORK = com.firebase.ui.auth.ResultCodes.NO_NETWORK;
+
+}
