2269cdf87ab5936b4f26943071922fc24a149d7b
Alex Saveau <gojunkmailx@gmail.com>
2016-12-01 18:09:24 -0800

    Fix merge mistakes

--- auth/src/main/java/com/firebase/ui/auth/util/signincontainer/IdpSignInContainer.java
+++ auth/src/main/java/com/firebase/ui/auth/util/signincontainer/IdpSignInContainer.java
@@ -74,7 +74,10 @@ public class IdpSignInContainer extends BaseFragment implements IdpCallback {
         }
 
         if (provider.equalsIgnoreCase(GoogleAuthProvider.PROVIDER_ID)) {
-            mIdpProvider = new GoogleProvider(getActivity(), providerConfig, email);
+            mIdpProvider = new GoogleProvider(
+                    getActivity(),
+                    providerConfig,
+                    getArguments().getString(ExtraConstants.EXTRA_EMAIL));
         } else if (provider.equalsIgnoreCase(FacebookAuthProvider.PROVIDER_ID)) {
             mIdpProvider = new FacebookProvider(getContext(), providerConfig);
         } else if (provider.equalsIgnoreCase(TwitterAuthProvider.PROVIDER_ID)) {
