ed4b11baf1ac0226e51f910438e23b7f81a97a82
Alex Saveau <saveau.alexandre@gmail.com>
2016-12-10 13:57:50 -0800

    Bypass twitter get email activity
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- auth/src/main/java/com/firebase/ui/auth/provider/TwitterProvider.java
+++ auth/src/main/java/com/firebase/ui/auth/provider/TwitterProvider.java
@@ -10,15 +10,29 @@ import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.R;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.TwitterAuthProvider;
+import com.google.gson.GsonBuilder;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.twitter.sdk.android.core.internal.TwitterApi;
+import com.twitter.sdk.android.core.internal.network.OkHttpClientHelper;
+import com.twitter.sdk.android.core.models.BindingValues;
+import com.twitter.sdk.android.core.models.BindingValuesAdapter;
+import com.twitter.sdk.android.core.models.SafeListAdapter;
+import com.twitter.sdk.android.core.models.SafeMapAdapter;
+import com.twitter.sdk.android.core.models.User;
 
 import io.fabric.sdk.android.Fabric;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 public class TwitterProvider extends Callback<TwitterSession> implements IdpProvider {
     private static final String TAG = "TwitterProvider";
@@ -61,17 +75,35 @@ public class TwitterProvider extends Callback<TwitterSession> implements IdpProv
 
     @Override
     public void success(final Result<TwitterSession> sessionResult) {
-        mTwitterAuthClient.requestEmail(sessionResult.data, new Callback<String>() {
-            @Override
-            public void success(Result<String> result) {
-                mCallbackObject.onSuccess(createIdpResponse(sessionResult.data, result.data));
-            }
+        new Retrofit.Builder()
+                .client(getClient(sessionResult))
+                .baseUrl(new TwitterApi().getBaseHostUrl())
+                .addConverterFactory(getFactory())
+                .build()
+                .create(EmailService.class)
+                .getEmail()
+                .enqueue(new Callback<User>() {
+                    @Override
+                    public void success(Result<User> result) {
+                        String email = result.data.email;
+                        if (email == null) {
+                            TwitterProvider.this.failure(
+                                    new TwitterException("Your application may not have access to"
+                                                                 + " email addresses or the user may not have an email address. To request"
+                                                                 + " access, please visit https://support.twitter.com/forms/platform."));
+                        } else if (email.equals("")) {
+                            TwitterProvider.this.failure(
+                                    new TwitterException("This user does not have an email address."));
+                        } else {
+                            mCallbackObject.onSuccess(createIdpResponse(sessionResult.data, email));
+                        }
+                    }
 
-            @Override
-            public void failure(TwitterException exception) {
-                TwitterProvider.this.failure(exception);
-            }
-        });
+                    @Override
+                    public void failure(TwitterException exception) {
+                        TwitterProvider.this.failure(exception);
+                    }
+                });
     }
 
     @Override
@@ -88,12 +120,31 @@ public class TwitterProvider extends Callback<TwitterSession> implements IdpProv
                 twitterSession.getAuthToken().secret);
     }
 
+    private OkHttpClient getClient(Result<TwitterSession> sessionResult) {
+        return OkHttpClientHelper.getOkHttpClient(
+                sessionResult.data,
+                TwitterCore.getInstance().getAuthConfig(),
+                TwitterCore.getInstance().getSSLSocketFactory());
+    }
+
+    private GsonConverterFactory getFactory() {
+        return GsonConverterFactory.create(
+                new GsonBuilder()
+                        .registerTypeAdapterFactory(new SafeListAdapter())
+                        .registerTypeAdapterFactory(new SafeMapAdapter())
+                        .registerTypeAdapter(BindingValues.class, new BindingValuesAdapter())
+                        .create());
+    }
+
+    interface EmailService {
+        @GET("/1.1/account/verify_credentials.json?include_email=true?include_entities=true?skip_status=true")
+        Call<User> getEmail();
+    }
+
     public static AuthCredential createAuthCredential(IdpResponse response) {
-        if (!response.getProviderType().equalsIgnoreCase(TwitterAuthProvider.PROVIDER_ID)){
+        if (!response.getProviderType().equalsIgnoreCase(TwitterAuthProvider.PROVIDER_ID)) {
             return null;
         }
-        return TwitterAuthProvider.getCredential(
-                response.getIdpToken(),
-                response.getIdpSecret());
+        return TwitterAuthProvider.getCredential(response.getIdpToken(), response.getIdpSecret());
     }
 }
