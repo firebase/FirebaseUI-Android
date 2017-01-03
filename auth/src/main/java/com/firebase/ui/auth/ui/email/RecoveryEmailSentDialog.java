1df7d3d2ea04044bdfbefbf311ea8982e1749593
Alex Saveau <SUPERCILEX@users.noreply.github.com>
2017-01-03 09:19:16 -0800

    Use native string formatter (#475)

--- auth/src/main/java/com/firebase/ui/auth/ui/email/RecoveryEmailSentDialog.java
+++ auth/src/main/java/com/firebase/ui/auth/ui/email/RecoveryEmailSentDialog.java
@@ -21,8 +21,8 @@ public class RecoveryEmailSentDialog extends DialogBase {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         return new AlertDialog.Builder(getContext(), R.style.FirebaseUI_Dialog)
                 .setTitle(R.string.title_confirm_recover_password_activity)
-                .setMessage(String.format(getString(R.string.confirm_recovery_body),
-                                          getArguments().getString(ExtraConstants.EXTRA_EMAIL)))
+                .setMessage(getString(R.string.confirm_recovery_body,
+                                      getArguments().getString(ExtraConstants.EXTRA_EMAIL)))
                 .setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface anInterface) {
