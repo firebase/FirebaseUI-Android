ab9b80118e5f1921d63e01397992626cc0a44af0
Alex Saveau <SUPERCILEX@users.noreply.github.com>
2017-01-03 08:29:48 -0800

    Rename BaseDialog and BaseFragment to be like other bases in FirebaseUI (#476)

--- auth/src/main/java/com/firebase/ui/auth/ui/FragmentBase.java
+++ auth/src/main/java/com/firebase/ui/auth/ui/FragmentBase.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
-public class BaseFragment extends Fragment {
+public class FragmentBase extends Fragment {
     protected FragmentHelper mHelper;
 
     @Override
