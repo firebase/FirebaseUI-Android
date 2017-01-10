f715502474d29dc4ebfc6c86c931731c7f39a9b8
puf <frank.vanpuffelen@gmail.com>
2017-01-10 11:23:38 -0800

    Added whitespace around ternary operator, because the style guide is picky about that.

--- app/src/main/java/com/firebase/uidemo/database/ChatActivity.java
+++ app/src/main/java/com/firebase/uidemo/database/ChatActivity.java
@@ -162,7 +162,7 @@ public class ChatActivity extends AppCompatActivity implements FirebaseAuth.Auth
             @Override
             protected void onDataChanged() {
                 // if there are no chat messages, show a view that invites the user to add a message
-                mEmptyListView.setVisibility(mRecyclerViewAdapter.getItemCount() == 0?View.VISIBLE:View.INVISIBLE);
+                mEmptyListView.setVisibility(mRecyclerViewAdapter.getItemCount() == 0 ? View.VISIBLE : View.INVISIBLE);
             }
         };
 
