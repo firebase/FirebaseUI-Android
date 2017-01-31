647b0dbc1e885dfc481c55c9f1a7a253f441e9cf
Alex Saveau <saveau.alexandre@gmail.com>
2017-01-30 17:28:31 -0800

    Force Facebook init for tests
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- auth/src/test/java/com/firebase/ui/auth/ui/idp/AuthMethodPickerActivityTest.java
+++ auth/src/test/java/com/firebase/ui/auth/ui/idp/AuthMethodPickerActivityTest.java
@@ -19,6 +19,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.facebook.FacebookSdk;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.BuildConfig;
 import com.firebase.ui.auth.R;
@@ -137,6 +138,7 @@ public class AuthMethodPickerActivityTest {
         List<String> providers = Arrays.asList(AuthUI.FACEBOOK_PROVIDER);
 
         AuthMethodPickerActivity authMethodPickerActivity = createActivity(providers);
+        FacebookSdk.sdkInitialize(authMethodPickerActivity);
 
         Button facebookButton = (Button) authMethodPickerActivity.findViewById(R.id.facebook_button);
         assertNotNull(facebookButton);
