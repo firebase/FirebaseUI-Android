2d0ff1168fc7df60da7cac1913346563e0fa4dee
Alex Saveau <saveau.alexandre@gmail.com>
2017-01-30 17:17:20 -0800

    Add permanently denied permission handling and sign result notification unification
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

---
+++ app/src/main/java/com/firebase/uidemo/util/SignInResultNotifier.java
@@ -0,0 +1,30 @@
+package com.firebase.uidemo.util;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.firebase.uidemo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+
+/**
+ * Notifies the user of sign in successes or failures beyond the lifecycle of an activity.
+ */
+public class SignInResultNotifier implements OnCompleteListener<AuthResult> {
+    private Context mContext;
+
+    public SignInResultNotifier(Context context) {
+        mContext = context.getApplicationContext();
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<AuthResult> task) {
+        if (task.isSuccessful()) {
+            Toast.makeText(mContext, R.string.signed_in, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(mContext, R.string.anonymous_auth_failed_msg, Toast.LENGTH_LONG).show();
+        }
+    }
+}
