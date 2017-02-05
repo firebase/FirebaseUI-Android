8dbdfa8a1c570f3f2355df78d20fa9bcebe2c7eb
Alex Saveau <saveau.alexandre@gmail.com>
2017-02-05 00:50:40 -0800

    Fix proguard saying SubscriptionEventListener.EventType doesn't exist
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- database/src/main/java/com/firebase/ui/database/SubscriptionEventListener.java
+++ database/src/main/java/com/firebase/ui/database/SubscriptionEventListener.java
@@ -1,11 +1,13 @@
 package com.firebase.ui.database;
 
 import android.support.annotation.IntDef;
+import android.support.annotation.Keep;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 public interface SubscriptionEventListener {
+    @Keep
     @Retention(RetentionPolicy.SOURCE)
     @IntDef({EventType.ADDED, EventType.REMOVED})
     @interface EventType {
