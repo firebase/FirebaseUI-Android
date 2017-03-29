cc95511bb52d0c620fe35910d9290c9bbfb13e4b
Aaron Mandle <amandle@google.com>
2017-03-29 16:52:28 -0700

    add ability to remove profile image

--- auth/src/main/java/com/firebase/ui/auth/ui/accountmanagement/EditDisplayNameActivity.java
+++ auth/src/main/java/com/firebase/ui/auth/ui/accountmanagement/EditDisplayNameActivity.java
@@ -2,15 +2,18 @@ package com.firebase.ui.auth.ui.accountmanagement;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.auth.R;
 import com.firebase.ui.auth.ui.BaseHelper;
 import com.firebase.ui.auth.ui.FlowParameters;
-import com.firebase.ui.auth.ui.HelperActivityBase;
 import com.firebase.ui.auth.ui.TaskFailureLogger;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -29,6 +32,16 @@ public class EditDisplayNameActivity extends SaveFieldActivity {
         setContentView(R.layout.activity_edit_display_name);
         mDisplayName = (EditText) findViewById(R.id.display_name);
         mDisplayName.setText(mActivityHelper.getCurrentUser().getDisplayName());
+
+        displayProfilePicture();
+
+        TextView removeLink = (TextView) findViewById(R.id.remove_profile_image_link);
+        removeLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeProfilePicture();
+            }
+        });
     }
 
     @Override
@@ -44,5 +57,30 @@ public class EditDisplayNameActivity extends SaveFieldActivity {
                     }
                 })
                 .addOnFailureListener(new TaskFailureLogger(TAG, "Failed to save display name"));
+    }
+
+    private void displayProfilePicture() {
+        ImageView profilePicture = (ImageView) findViewById(R.id.profile_image);
+        Uri profilePhoto = mActivityHelper.getCurrentUser().getPhotoUrl();
+        Glide.with(this)
+                .load(profilePhoto)
+                .placeholder(R.drawable.ic_person_white_48dp)
+                .transform(new CircleTransform(getApplicationContext()))
+                .into(profilePicture);
+    }
+
+    private void removeProfilePicture() {
+        mActivityHelper.getCurrentUser().updateProfile(
+                new UserProfileChangeRequest.Builder()
+                .setPhotoUri(null)
+                .build())
+        .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.e(TAG, "successfully removed profile image");
+                displayProfilePicture();
+            }
+        })
+        .addOnFailureListener(new TaskFailureLogger(TAG, "unable to remove profile image"));
     }
 }
