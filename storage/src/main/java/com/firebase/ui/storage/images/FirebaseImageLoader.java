13d2444c1ed55812c9ae596da6f640feb5441590
Alex Saveau <saveau.alexandre@gmail.com>
2017-04-24 17:58:03 -0700

    Fix quality checks

--- storage/src/main/java/com/firebase/ui/storage/images/FirebaseImageLoader.java
+++ storage/src/main/java/com/firebase/ui/storage/images/FirebaseImageLoader.java
@@ -48,6 +48,7 @@ public class FirebaseImageLoader implements StreamModelLoader<StorageReference> 
         @Override
         public InputStream loadData(Priority priority) throws Exception {
             mStreamTask = mRef.getStream();
+            //noinspection VisibleForTests TODO remove once the tools team fixes lint
             mInputStream = Tasks.await(mStreamTask).getStream();
 
             return mInputStream;
