8db5e52442a78f769e624d8049498ccbd3bff15c
Alex Saveau <saveau.alexandre@gmail.com>
2017-08-10 11:08:33 -0700

    Simplify ProfileMerger.java
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- auth/src/main/java/com/firebase/ui/auth/util/accountlink/ProfileMerger.java
+++ auth/src/main/java/com/firebase/ui/auth/util/accountlink/ProfileMerger.java
@@ -8,9 +8,7 @@ import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.User;
 import com.firebase.ui.auth.ui.TaskFailureLogger;
 import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,13 +19,10 @@ import com.google.firebase.auth.UserProfileChangeRequest;
  * <p>
  * <b>Note:</b> This operation always returns a successful task to minimize login interruptions.
  */
-public class ProfileMerger implements Continuation<AuthResult, Task<AuthResult>>, OnCompleteListener<Void> {
+public class ProfileMerger implements Continuation<AuthResult, Task<AuthResult>> {
     private static final String TAG = "ProfileMerger";
 
-    private final TaskCompletionSource<AuthResult> mProfileUpdateCompleteTask = new TaskCompletionSource<>();
     private final IdpResponse mIdpResponse;
-
-    private AuthResult mAuthResult;
 
     public ProfileMerger(IdpResponse response) {
         mIdpResponse = response;
@@ -35,32 +30,30 @@ public class ProfileMerger implements Continuation<AuthResult, Task<AuthResult>>
 
     @Override
     public Task<AuthResult> then(@NonNull Task<AuthResult> task) throws Exception {
-        mAuthResult = task.getResult();
-        FirebaseUser firebaseUser = mAuthResult.getUser();
+        final AuthResult authResult = task.getResult();
+        FirebaseUser firebaseUser = authResult.getUser();
 
         String name = firebaseUser.getDisplayName();
         Uri photoUri = firebaseUser.getPhotoUrl();
         if (!TextUtils.isEmpty(name) && photoUri != null) {
-            return Tasks.forResult(mAuthResult);
+            return Tasks.forResult(authResult);
         }
 
         User user = mIdpResponse.getUser();
         if (TextUtils.isEmpty(name)) { name = user.getName(); }
         if (photoUri == null) { photoUri = user.getPhotoUri(); }
 
-        firebaseUser.updateProfile(
+        return firebaseUser.updateProfile(
                 new UserProfileChangeRequest.Builder()
                         .setDisplayName(name)
                         .setPhotoUri(photoUri)
                         .build())
-                .addOnCompleteListener(this)
-                .addOnFailureListener(new TaskFailureLogger(TAG, "Error updating profile"));
-
-        return mProfileUpdateCompleteTask.getTask();
-    }
-
-    @Override
-    public void onComplete(@NonNull Task<Void> task) {
-        mProfileUpdateCompleteTask.setResult(mAuthResult);
+                .addOnFailureListener(new TaskFailureLogger(TAG, "Error updating profile"))
+                .continueWithTask(new Continuation<Void, Task<AuthResult>>() {
+                    @Override
+                    public Task<AuthResult> then(@NonNull Task<Void> task) throws Exception {
+                        return Tasks.forResult(authResult);
+                    }
+                });
     }
 }
