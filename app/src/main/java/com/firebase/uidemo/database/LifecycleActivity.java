c458630b90c073a349d6ec6c198ad7cb557ad42e
Alex Saveau <saveau.alexandre@gmail.com>
2017-09-11 17:41:02 -0700

    Fix lint
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/firebase/uidemo/database/LifecycleActivity.java
+++ app/src/main/java/com/firebase/uidemo/database/LifecycleActivity.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.support.v7.app.AppCompatActivity;
 
 // TODO remove once arch components are merged into support lib
+@SuppressWarnings("Registered")
 public class LifecycleActivity extends AppCompatActivity implements LifecycleRegistryOwner {
     private final LifecycleRegistry mRegistry = new LifecycleRegistry(this);
 
