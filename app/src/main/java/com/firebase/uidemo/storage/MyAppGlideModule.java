162663871acc1dcf8c21553e62ae12c5c9ede5a5
Sam Stern <samstern@google.com>
2017-09-19 16:56:37 -0700

    Update app dependencies
    Change-Id: I2d190c5ced6cb06ea9b9b93ecc5e0886855efecc

--- app/src/main/java/com/firebase/uidemo/storage/MyAppGlideModule.java
+++ app/src/main/java/com/firebase/uidemo/storage/MyAppGlideModule.java
@@ -2,6 +2,7 @@ package com.firebase.uidemo.storage;
 
 import android.content.Context;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
@@ -18,7 +19,7 @@ import java.io.InputStream;
 public class MyAppGlideModule extends AppGlideModule {
 
     @Override
-    public void registerComponents(Context context, Registry registry) {
+    public void registerComponents(Context context, Glide glide, Registry registry) {
         // Register FirebaseImageLoader to handle StorageReference
         registry.append(StorageReference.class, InputStream.class,
                 new FirebaseImageLoader.Factory());
