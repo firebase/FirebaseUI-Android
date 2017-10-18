965c3a2dfeb34664ce514a2338c07073ee8fdf71
Abdul Wasae <awasae3@gmail.com>
2017-10-18 09:53:55 +0500

    Fix NPE when dismissing loading dialog (#985)

--- auth/src/main/java/com/firebase/ui/auth/ui/phone/PhoneVerificationActivity.java
+++ auth/src/main/java/com/firebase/ui/auth/ui/phone/PhoneVerificationActivity.java
@@ -394,7 +394,7 @@ public class PhoneVerificationActivity extends AppCompatBase {
     }
 
     private void dismissLoadingDialog() {
-        if (mProgressDialog != null) {
+        if (mProgressDialog != null && !isFinishing()) {
             mProgressDialog.dismissAllowingStateLoss();
             mProgressDialog = null;
         }
