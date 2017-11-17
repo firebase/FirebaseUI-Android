firebase/FirebaseUI-Android

commit 7ca3cbf112854b63f6a980e0b2aef8294a67e67c

Rewrite relevant parts of the public API (#1010)

auth/src/main/java/com/firebase/ui/auth/AuthUI.java
@@ -14,7 +14,7 @@
 
 package com.firebase.ui.auth;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcel;
@@ -23,20 +23,25 @@ import android.support.annotation.CallSuper;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RestrictTo;
 import android.support.annotation.StringDef;
 import android.support.annotation.StyleRes;
-import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
 
 import com.facebook.login.LoginManager;
 import com.firebase.ui.auth.provider.TwitterProvider;
 import com.firebase.ui.auth.ui.ExtraConstants;
 import com.firebase.ui.auth.ui.FlowParameters;
 import com.firebase.ui.auth.ui.idp.AuthMethodPickerActivity;
-import com.firebase.ui.auth.util.GoogleSignInHelper;
+import com.firebase.ui.auth.util.GoogleApiUtils;
 import com.firebase.ui.auth.util.Preconditions;
-import com.firebase.ui.auth.util.signincontainer.SmartLockBase;
+import com.firebase.ui.auth.util.signincontainer.SaveSmartLock;
 import com.google.android.gms.auth.api.credentials.Credential;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.auth.api.credentials.CredentialsClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.Tasks;
@@ -44,10 +49,13 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthProvider;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.auth.TwitterAuthProvider;
+import com.google.firebase.auth.UserInfo;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -138,6 +146,16 @@ public class AuthUI {
                     PHONE_VERIFICATION_PROVIDER
             )));
 
+    /**
+     * The set of social authentication providers supported in Firebase Auth UI.
+     */
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    public static final Set<String> SOCIAL_PROVIDERS =
+            Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+                    GoogleAuthProvider.PROVIDER_ID,
+                    FacebookAuthProvider.PROVIDER_ID,
+                    TwitterAuthProvider.PROVIDER_ID)));
+
     private static final IdentityHashMap<FirebaseApp, AuthUI> INSTANCES = new IdentityHashMap<>();
 
     private final FirebaseApp mApp;
@@ -189,40 +207,17 @@ public class AuthUI {
     /**
      * Signs the current user out, if one is signed in.
      *
-     * @param activity the activity requesting the user be signed out
+     * @param context the context requesting the user be signed out
      * @return A task which, upon completion, signals that the user has been signed out ({@link
      * Task#isSuccessful()}, or that the sign-out attempt failed unexpectedly !{@link
      * Task#isSuccessful()}).
      */
     @NonNull
-    public Task<Void> signOut(@NonNull FragmentActivity activity) {
-        // Get Credentials Helper
-        GoogleSignInHelper signInHelper = GoogleSignInHelper.getInstance(activity);
-
-        // Firebase Sign out
+    public Task<Void> signOut(@NonNull Context context) {
         mAuth.signOut();
-
-        // Disable credentials auto sign-in
-        Task<Status> disableCredentialsTask = signInHelper.disableAutoSignIn();
-
-        // Google sign out
-        Task<Status> signOutTask = signInHelper.signOut();
-
-        // Facebook sign out
-        try {
-            LoginManager.getInstance().logOut();
-        } catch (NoClassDefFoundError e) {
-            // do nothing
-        }
-
-        // Twitter sign out
-        try {
-            TwitterProvider.signOut(activity);
-        } catch (NoClassDefFoundError e) {
-            // do nothing
-        }
-        // Wait for all tasks to complete
-        return Tasks.whenAll(disableCredentialsTask, signOutTask);
+        return Tasks.whenAll(
+                signOutIdps(context),
+                GoogleApiUtils.getCredentialsClient(context).disableAutoSignIn());
     }
 
     /**
@@ -231,48 +226,94 @@ public class AuthUI {
      * fails if the Firebase Auth deletion fails. Credentials deletion failures are handled
      * silently.
      *
-     * @param activity the calling {@link Activity}.
+     * @param context the calling {@link Context}.
      */
     @NonNull
-    public Task<Void> delete(@NonNull FragmentActivity activity) {
-        // Initialize SmartLock helper
-        GoogleSignInHelper signInHelper = GoogleSignInHelper.getInstance(activity);
-
-        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (firebaseUser == null) {
-            // If the current user is null, return a failed task immediately
-            return Tasks.forException(new Exception("No currently signed in user."));
+    public Task<Void> delete(@NonNull Context context) {
+        final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            return Tasks.forException(new FirebaseAuthInvalidUserException(
+                    String.valueOf(CommonStatusCodes.SIGN_IN_REQUIRED),
+                    "No currently signed in user."));
         }
 
-        // Delete the Firebase user
-        Task<Void> deleteUserTask = firebaseUser.delete();
+        final List<Credential> credentials = getCredentialsFromFirebaseUser(currentUser);
+        final CredentialsClient client = GoogleApiUtils.getCredentialsClient(context);
 
-        // Get all SmartLock credentials associated with the user
-        List<Credential> credentials = SmartLockBase.credentialsFromFirebaseUser(firebaseUser);
-
-        // For each Credential in the list, create a task to delete it.
-        List<Task<?>> credentialTasks = new ArrayList<>();
-        for (Credential credential : credentials) {
-            credentialTasks.add(signInHelper.delete(credential));
-        }
-
-        // Create a combined task that will succeed when all credential delete operations
-        // have completed (even if they fail).
-        final Task<Void> combinedCredentialTask = Tasks.whenAll(credentialTasks);
-
-        // Chain the Firebase Auth delete task with the combined Credentials task
-        // and return.
-        return deleteUserTask.continueWithTask(new Continuation<Void, Task<Void>>() {
+        // Ensure the order in which tasks are executed properly destructures the user.
+        return signOutIdps(context).continueWithTask(new Continuation<Void, Task<Void>>() {
             @Override
-            public Task<Void> then(@NonNull Task<Void> task) throws Exception {
-                // Call getResult() to propagate failure by throwing an exception
-                // if there was one.
-                task.getResult(Exception.class);
+            public Task<Void> then(@NonNull Task<Void> task) {
+                task.getResult(); // Propagate exception if there was one
+                return currentUser.delete();
+            }
+        }).continueWithTask(new Continuation<Void, Task<Void>>() {
+            @Override
+            public Task<Void> then(@NonNull Task<Void> task) {
+                task.getResult(); // Propagate exception if there was one
 
-                // Return the combined credential task
-                return combinedCredentialTask;
+                List<Task<?>> credentialTasks = new ArrayList<>();
+                for (Credential credential : credentials) {
+                    credentialTasks.add(client.delete(credential));
+                }
+                return Tasks.whenAll(credentialTasks)
+                        .continueWithTask(new Continuation<Void, Task<Void>>() {
+                            @Override
+                            public Task<Void> then(@NonNull Task<Void> task) {
+                                Exception e = task.getException();
+                                Throwable t = e == null ? null : e.getCause();
+                                if (!(t instanceof ApiException)
+                                        || ((ApiException) t).getStatusCode() != CommonStatusCodes.CANCELED) {
+                                    // Only propagate the exception if it isn't an invalid account
+                                    // one. This can occur if we failed to save the credential or it
+                                    // was deleted elsewhere. However, a lack of stored credential
+                                    // doesn't mean fully deleting the user failed.
+                                    task.getResult();
+                                }
+
+                                return Tasks.forResult(null);
+                            }
+                        });
             }
         });
+    }
+
+    private Task<Void> signOutIdps(@NonNull Context context) {
+        try {
+            LoginManager.getInstance().logOut();
+            TwitterProvider.signOut(context);
+        } catch (NoClassDefFoundError e) {
+            // Do nothing: this is perfectly fine if the dev doesn't include Facebook/Twitter
+            // support
+        }
+
+        return GoogleSignIn.getClient(context, GoogleSignInOptions.DEFAULT_SIGN_IN).signOut();
+    }
+
+    /**
+     * Make a list of {@link Credential} from a FirebaseUser. Useful for deleting Credentials, not
+     * for saving since we don't have access to the password.
+     */
+    private static List<Credential> getCredentialsFromFirebaseUser(@NonNull FirebaseUser user) {
+        if (TextUtils.isEmpty(user.getEmail()) && TextUtils.isEmpty(user.getPhoneNumber())) {
+            return Collections.emptyList();
+        }
+
+        List<Credential> credentials = new ArrayList<>();
+        for (UserInfo userInfo : user.getProviderData()) {
+            if (FirebaseAuthProvider.PROVIDER_ID.equals(userInfo.getProviderId())) {
+                continue;
+            }
+
+            String type = SaveSmartLock.providerIdToAccountType(userInfo.getProviderId());
+
+            credentials.add(new Credential.Builder(
+                    user.getEmail() == null ? user.getPhoneNumber() : user.getEmail())
+                    .setAccountType(type)
+                    .build());
+        }
+
+        return credentials;
     }
 
     /**

app/src/main/java/com/firebase/uidemo/auth/AuthUiActivity.java
@@ -148,13 +148,6 @@ public class AuthUiActivity extends AppCompatActivity {
         setContentView(R.layout.auth_ui_layout);
         ButterKnife.bind(this);
 
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        if (auth.getCurrentUser() != null) {
-            startSignedInActivity(null);
-            finish();
-            return;
-        }
-
         if (isGoogleMisconfigured()) {
             mUseGoogleProvider.setChecked(false);
             mUseGoogleProvider.setEnabled(false);
@@ -225,6 +218,16 @@ public class AuthUiActivity extends AppCompatActivity {
         }
 
         showSnackbar(R.string.unknown_response);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startSignedInActivity(null);
+            finish();
+        }
     }
 
     @MainThread

app/src/main/java/com/firebase/uidemo/auth/SignedInActivity.java
@@ -43,6 +43,8 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.auth.TwitterAuthProvider;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -187,14 +189,24 @@ public class SignedInActivity extends AppCompatActivity {
             Iterator<String> providerIter = user.getProviders().iterator();
             while (providerIter.hasNext()) {
                 String provider = providerIter.next();
-                if (GoogleAuthProvider.PROVIDER_ID.equals(provider)) {
-                    providerList.append("Google");
-                } else if (FacebookAuthProvider.PROVIDER_ID.equals(provider)) {
-                    providerList.append("Facebook");
-                } else if (EmailAuthProvider.PROVIDER_ID.equals(provider)) {
-                    providerList.append("Password");
-                } else {
-                    providerList.append(provider);
+                switch (provider) {
+                    case GoogleAuthProvider.PROVIDER_ID:
+                        providerList.append("Google");
+                        break;
+                    case FacebookAuthProvider.PROVIDER_ID:
+                        providerList.append("Facebook");
+                        break;
+                    case TwitterAuthProvider.PROVIDER_ID:
+                        providerList.append("Twitter");
+                        break;
+                    case EmailAuthProvider.PROVIDER_ID:
+                        providerList.append("Email");
+                        break;
+                    case PhoneAuthProvider.PROVIDER_ID:
+                        providerList.append("Phone");
+                        break;
+                    default:
+                        throw new IllegalStateException("Unknown provider: " + provider);
                 }
 
                 if (providerIter.hasNext()) {

auth/src/main/java/com/firebase/ui/auth/IdpResponse.java
@@ -23,9 +23,8 @@ import android.support.annotation.Nullable;
 import android.support.annotation.RestrictTo;
 import android.text.TextUtils;
 
+import com.firebase.ui.auth.data.model.FirebaseUiException;
 import com.firebase.ui.auth.ui.ExtraConstants;
-import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.TwitterAuthProvider;
 
 /**
@@ -33,20 +32,32 @@ import com.google.firebase.auth.TwitterAuthProvider;
  */
 public class IdpResponse implements Parcelable {
     private final User mUser;
+
     private final String mToken;
     private final String mSecret;
 
-    private final int mErrorCode;
+    private final FirebaseUiException mException;
 
-    private IdpResponse(int errorCode) {
-        this(null, null, null, errorCode);
+    private IdpResponse(@NonNull FirebaseUiException e) {
+        this(null, null, null, e);
     }
 
-    private IdpResponse(User user, String token, String secret, int errorCode) {
+    private IdpResponse(
+            @NonNull User user,
+            @Nullable String token,
+            @Nullable String secret) {
+        this(user, token, secret, null);
+    }
+
+    private IdpResponse(
+            User user,
+            String token,
+            String secret,
+            FirebaseUiException e) {
         mUser = user;
         mToken = token;
         mSecret = secret;
-        mErrorCode = errorCode;
+        mException = e;
     }
 
     /**
@@ -65,13 +76,27 @@ public class IdpResponse implements Parcelable {
     }
 
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    public static IdpResponse fromError(@NonNull FirebaseUiException e) {
+        return new IdpResponse(e);
+    }
+
+    /**
+     * @deprecated migrate internals to {@link #fromError(FirebaseUiException)}
+     */
+    @Deprecated
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static Intent getErrorCodeIntent(int errorCode) {
-        return new IdpResponse(errorCode).toIntent();
+        return new IdpResponse(new FirebaseUiException(errorCode)).toIntent();
     }
 
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public Intent toIntent() {
         return new Intent().putExtra(ExtraConstants.EXTRA_IDP_RESPONSE, this);
+    }
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    public boolean isSuccessful() {
+        return mException == null;
     }
 
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
@@ -124,7 +149,17 @@ public class IdpResponse implements Parcelable {
      * Get the error code for a failed sign in
      */
     public int getErrorCode() {
-        return mErrorCode;
+        if (isSuccessful()) {
+            return Activity.RESULT_OK;
+        } else {
+            return mException.getErrorCode();
+        }
+    }
+
+    @Nullable
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    public FirebaseUiException getException() {
+        return mException;
     }
 
     @Override
@@ -137,7 +172,7 @@ public class IdpResponse implements Parcelable {
         dest.writeParcelable(mUser, flags);
         dest.writeString(mToken);
         dest.writeString(mSecret);
-        dest.writeInt(mErrorCode);
+        dest.writeSerializable(mException);
     }
 
     public static final Creator<IdpResponse> CREATOR = new Creator<IdpResponse>() {
@@ -147,7 +182,7 @@ public class IdpResponse implements Parcelable {
                     in.<User>readParcelable(User.class.getClassLoader()),
                     in.readString(),
                     in.readString(),
-                    in.readInt()
+                    (FirebaseUiException) in.readSerializable()
             );
         }
 
@@ -159,7 +194,8 @@ public class IdpResponse implements Parcelable {
 
     @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
     public static class Builder {
-        private User mUser;
+        private final User mUser;
+
         private String mToken;
         private String mSecret;
 
@@ -179,20 +215,20 @@ public class IdpResponse implements Parcelable {
 
         public IdpResponse build() {
             String providerId = mUser.getProviderId();
-            if ((providerId.equalsIgnoreCase(GoogleAuthProvider.PROVIDER_ID)
-                    || providerId.equalsIgnoreCase(FacebookAuthProvider.PROVIDER_ID)
-                    || providerId.equalsIgnoreCase(TwitterAuthProvider.PROVIDER_ID))
-                    && TextUtils.isEmpty(mToken)) {
+            if (!AuthUI.SUPPORTED_PROVIDERS.contains(providerId)) {
+                throw new IllegalStateException("Unknown provider: " + providerId);
+            }
+            if (AuthUI.SOCIAL_PROVIDERS.contains(providerId) && TextUtils.isEmpty(mToken)) {
                 throw new IllegalStateException(
                         "Token cannot be null when using a non-email provider.");
             }
-            if (providerId.equalsIgnoreCase(TwitterAuthProvider.PROVIDER_ID)
+            if (providerId.equals(TwitterAuthProvider.PROVIDER_ID)
                     && TextUtils.isEmpty(mSecret)) {
                 throw new IllegalStateException(
                         "Secret cannot be null when using the Twitter provider.");
             }
 
-            return new IdpResponse(mUser, mToken, mSecret, Activity.RESULT_OK);
+            return new IdpResponse(mUser, mToken, mSecret);
         }
     }
 }

auth/src/main/java/com/firebase/ui/auth/data/model/FirebaseUiException.java
@@ -1,0 +1,31 @@
+package com.firebase.ui.auth.data.model;
+
+/**
+ * Base class for all FirebaseUI exceptions.
+ */
+public class FirebaseUiException extends Exception {
+    private final int mErrorCode;
+
+    public FirebaseUiException(int code) {
+        mErrorCode = code;
+    }
+
+    public FirebaseUiException(int code, String message) {
+        super(message);
+        mErrorCode = code;
+    }
+
+    public FirebaseUiException(int code, String message, Throwable cause) {
+        super(message, cause);
+        mErrorCode = code;
+    }
+
+    public FirebaseUiException(int code, Throwable cause) {
+        super(cause);
+        mErrorCode = code;
+    }
+
+    public final int getErrorCode() {
+        return mErrorCode;
+    }
+}

auth/src/main/java/com/firebase/ui/auth/util/GoogleApiUtils.java
@@ -1,0 +1,27 @@
+package com.firebase.ui.auth.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.RestrictTo;
+
+import com.google.android.gms.auth.api.credentials.Credentials;
+import com.google.android.gms.auth.api.credentials.CredentialsClient;
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+public final class GoogleApiUtils {
+    private GoogleApiUtils() {
+        throw new AssertionError("No instance for you!");
+    }
+
+    @NonNull
+    public static CredentialsClient getCredentialsClient(@NonNull Context context) {
+        CredentialsClient client;
+        if (context instanceof Activity) {
+            client = Credentials.getClient((Activity) context);
+        } else {
+            client = Credentials.getClient(context);
+        }
+        return client;
+    }
+}

auth/src/main/java/com/firebase/ui/auth/util/signincontainer/SmartLockBase.java
@@ -4,14 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
-import android.util.Log;
 import android.util.Pair;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ui.FragmentBase;
-import com.google.android.gms.auth.api.credentials.Credential;
 import com.google.android.gms.auth.api.credentials.IdentityProviders;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -19,22 +16,14 @@ import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.auth.TwitterAuthProvider;
-import com.google.firebase.auth.UserInfo;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public abstract class SmartLockBase<R extends Result> extends FragmentBase implements
         GoogleApiClient.ConnectionCallbacks,
         ResultCallback<R>,
         GoogleApiClient.OnConnectionFailedListener {
-    private static final String TAG = "SmartLockBase";
-
     protected GoogleApiClient mGoogleApiClient;
 
     private boolean mWasProgressDialogShowing;
@@ -75,40 +64,6 @@ public abstract class SmartLockBase<R extends Result> extends FragmentBase imple
             default:
                 return null;
         }
-    }
-
-    /**
-     * Make a list of {@link Credential} from a FirebaseUser. Useful for deleting Credentials, not
-     * for saving since we don't have access to the password.
-     */
-    public static List<Credential> credentialsFromFirebaseUser(@NonNull FirebaseUser user) {
-        if (TextUtils.isEmpty(user.getEmail())) {
-            Log.w(TAG, "Can't get credentials from user with no email: " + user);
-            return Collections.emptyList();
-        }
-
-        List<Credential> credentials = new ArrayList<>();
-        for (UserInfo userInfo : user.getProviderData()) {
-            // Get provider ID from Firebase Auth
-            @AuthUI.SupportedProvider String providerId = userInfo.getProviderId();
-
-            // Convert to Credentials API account type
-            String accountType = providerIdToAccountType(providerId);
-
-            // Build and add credential
-            Credential.Builder builder = new Credential.Builder(user.getEmail())
-                    .setAccountType(accountType);
-
-            // Null account type means password, we need to add a random password
-            // to make deletion succeed.
-            if (accountType == null) {
-                builder.setPassword("some_password");
-            }
-
-            credentials.add(builder.build());
-        }
-
-        return credentials;
     }
 
     @Override

commit 26e8670295a7b7774613ef19bf9f1906110b0e07

Fix oversight introduced in 7ca3cbf112854b63f6a980e0b2aef8294a67e67c

auth/src/main/java/com/firebase/ui/auth/AuthUI.java
@@ -281,10 +281,18 @@ public class AuthUI {
     private Task<Void> signOutIdps(@NonNull Context context) {
         try {
             LoginManager.getInstance().logOut();
-            TwitterProvider.signOut(context);
         } catch (NoClassDefFoundError e) {
             // Do nothing: this is perfectly fine if the dev doesn't include Facebook/Twitter
             // support
+        }
+
+        try {
+            TwitterProvider.signOut(context);
+        } catch (NoClassDefFoundError e) {
+            // See comment above
+            // Note: we need to have speperate try/catch statements since devs can include
+            // _either_ one of the providers. If one crashes, we still need to sign out of
+            // the other one.
         }
 
         return GoogleSignIn.getClient(context, GoogleSignInOptions.DEFAULT_SIGN_IN).signOut();