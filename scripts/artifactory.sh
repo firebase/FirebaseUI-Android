9d6b683c00811ba521f9728ded449a808e85307e
Sam Stern <samstern@google.com>
2018-02-14 10:36:03 -0800

    Enable artifactory publication (#1146)

---
+++ scripts/artifactory.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+./gradlew :library:prepareArtifacts
+./gradlew :common:assembleRelease :common:prepareArtifacts :common:artifactoryPublish
+./gradlew :auth:assembleRelease :auth:prepareArtifacts :auth:artifactoryPublish
+./gradlew :database:assembleRelease :database:prepareArtifacts :database:artifactoryPublish
+./gradlew :firestore:assembleRelease :firestore:prepareArtifacts :firestore:artifactoryPublish
+./gradlew :storage:assembleRelease :storage:prepareArtifacts :storage:artifactoryPublish
