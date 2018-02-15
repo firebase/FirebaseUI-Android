0e34474b930da4c3c6364e47360c5d6c78f667a3
Sam Stern <samstern@google.com>
2018-02-15 14:15:35 -0800

    Auto-artifactory (#1152)

--- scripts/artifactory.sh
+++ scripts/artifactory.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+VERSION_BRANCH_RE="version-[0-9\.]+-dev"
+
+if [[ ($CI == "true") && !("${TRAVIS_BRANCH}" =~ $VERSION_BRANCH_RE) ]]; then
+    echo "Not triggering artifactory for branch: ${TRAVIS_BRANCH}"
+    exit 0
+fi
+
+if [[ ($CI == "true") && ($TRAVIS_EVENT_TYPE != "push") ]]; then
+    echo "Artifactory only triggered on 'push' builds."
+    exit 0
+fi
+
+
 ./gradlew :library:prepareArtifacts
 ./gradlew :common:assembleRelease :common:prepareArtifacts :common:artifactoryPublish
 ./gradlew :auth:assembleRelease :auth:prepareArtifacts :auth:artifactoryPublish
