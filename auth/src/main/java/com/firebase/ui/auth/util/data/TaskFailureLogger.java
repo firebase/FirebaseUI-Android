6f0e2c9adb8fec03358bdbeab7485ccfdf5d57ae
Alex Saveau <saveau.alexandre@gmail.com>
2018-03-15 17:32:29 -0700

    Start addressing review feedback
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- auth/src/main/java/com/firebase/ui/auth/util/data/TaskFailureLogger.java
+++ auth/src/main/java/com/firebase/ui/auth/util/data/TaskFailureLogger.java
@@ -3,28 +3,11 @@ package com.firebase.ui.auth.util.data;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.BuildConfig;
 import com.google.android.gms.tasks.OnFailureListener;
 
 public class TaskFailureLogger implements OnFailureListener {
-    private static final boolean LOG;
-
     private String mTag;
     private String mMessage;
-
-    static {
-        boolean log;
-        try {
-            Class<?> buildConfigClass = Class.forName(
-                    AuthUI.getApplicationContext().getPackageName() + "."
-                            + BuildConfig.class.getSimpleName());
-            log = buildConfigClass.getDeclaredField("DEBUG").getBoolean(null);
-        } catch (Exception ignored) {
-            log = false;
-        }
-        LOG = log;
-    }
 
     public TaskFailureLogger(@NonNull String tag, @NonNull String message) {
         mTag = tag;
@@ -33,6 +16,6 @@ public class TaskFailureLogger implements OnFailureListener {
 
     @Override
     public void onFailure(@NonNull Exception e) {
-        if (LOG) { Log.w(mTag, mMessage, e); }
+        Log.w(mTag, mMessage, e);
     }
 }
