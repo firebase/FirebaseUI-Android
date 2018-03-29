bcb873b119e675bf6a7252c79561333009ae642a
Alex Saveau <saveau.alexandre@gmail.com>
2018-03-29 14:57:26 -0700

    Fix possible memory leak
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- app/src/main/java/com/firebase/uidemo/auth/AuthUiActivity.java
+++ app/src/main/java/com/firebase/uidemo/auth/AuthUiActivity.java
@@ -171,7 +171,7 @@ public class AuthUiActivity extends AppCompatActivity {
     @OnClick(R.id.sign_in_silent)
     public void silentSignIn(View view) {
         AuthUI.getInstance().silentSignIn(this, getSelectedProviders())
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
