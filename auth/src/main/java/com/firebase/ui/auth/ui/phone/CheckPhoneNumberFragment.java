firebase/FirebaseUI-Android

commit a92d058ab498b626d00172d807a4ba0480de5846

Make country spinner survive rotation (#1520)

auth/src/main/java/com/firebase/ui/auth/ui/phone/CheckPhoneNumberFragment.java
@@ -98,6 +98,7 @@ public class CheckPhoneNumberFragment extends FragmentBase implements View.OnCli
         mSubmitButton.setOnClickListener(this);
 
         setupPrivacyDisclosures(view.<TextView>findViewById(R.id.email_footer_tos_and_pp_text));
+        setupCountrySpinner();
     }
 
     @Override
@@ -115,12 +116,12 @@ public class CheckPhoneNumberFragment extends FragmentBase implements View.OnCli
             }
         });
 
-        if (savedInstanceState != null || mCalled) { return; }
+        if (savedInstanceState != null || mCalled) {
+            return;
+        }
         // Fragment back stacks are the stuff of nightmares (what's new?): the fragment isn't
         // destroyed so its state isn't saved and we have to rely on an instance field. Sigh.
         mCalled = true;
-
-        setupCountrySpinner();
     }
 
     @Override

commit 46474e3988c49d70722f1806d3d98121df01cec6

@samtstern :troll::grin: (fix bugs introduced in a92d058ab498b626d00172d807a4ba0480de5846)
Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

auth/src/main/java/com/firebase/ui/auth/ui/phone/CheckPhoneNumberFragment.java
@@ -122,6 +122,9 @@ public class CheckPhoneNumberFragment extends FragmentBase implements View.OnCli
         // Fragment back stacks are the stuff of nightmares (what's new?): the fragment isn't
         // destroyed so its state isn't saved and we have to rely on an instance field. Sigh.
         mCalled = true;
+
+        // DON'T REMOVE
+        setDefaultCountryForSpinner();
     }
 
     @Override
@@ -196,8 +199,6 @@ public class CheckPhoneNumberFragment extends FragmentBase implements View.OnCli
     private void setupCountrySpinner() {
         Bundle params = getArguments().getBundle(ExtraConstants.PARAMS);
         mCountryListSpinner.init(params);
-
-        setDefaultCountryForSpinner();
 
         // Clear error when spinner is clicked on
         mCountryListSpinner.setOnClickListener(new View.OnClickListener() {