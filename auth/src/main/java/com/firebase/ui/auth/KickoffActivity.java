659bd11ff6c5d7ee60f692ac6d1976c934cdf8ab
Sam Stern <samstern@google.com>
2019-02-25 09:55:42 -0800

    Remove offline check
    Change-Id: I37628cc202065221dab9626aa642871f9d527703

--- auth/src/main/java/com/firebase/ui/auth/KickoffActivity.java
+++ auth/src/main/java/com/firebase/ui/auth/KickoffActivity.java
@@ -3,7 +3,6 @@ package com.firebase.ui.auth;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -62,14 +61,7 @@ public class KickoffActivity extends InvisibleActivityBase {
                             return;
                         }
 
-                        boolean needsInternet = getFlowParams().enableCredentials
-                                && !getFlowParams().shouldShowProviderChoice();
-                        if (isOffline() && needsInternet) {
-                            finish(RESULT_CANCELED, IdpResponse.getErrorIntent(
-                                    new FirebaseUiException(ErrorCodes.NO_NETWORK)));
-                        } else {
-                            mKickstarter.start();
-                        }
+                        mKickstarter.start();
                     }
                 })
                 .addOnFailureListener(this, new OnFailureListener() {
