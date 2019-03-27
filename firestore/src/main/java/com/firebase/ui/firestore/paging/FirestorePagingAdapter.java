ef858c773c0b63eb5d733da51a09869e60de29ae
Shreyas Patil <shreyaspatilg@gmail.com>
2019-03-28 00:26:56 +0530

    Fixed retry() and added refresh() as #1608

--- firestore/src/main/java/com/firebase/ui/firestore/paging/FirestorePagingAdapter.java
+++ firestore/src/main/java/com/firebase/ui/firestore/paging/FirestorePagingAdapter.java
@@ -34,6 +34,14 @@ public abstract class FirestorePagingAdapter<T, VH extends RecyclerView.ViewHold
     private final LiveData<LoadingState> mLoadingState;
     private final LiveData<FirestoreDataSource> mDataSource;
 
+
+    private final Observer<FirestoreDataSource> mDataSourceObserver = new Observer<FirestoreDataSource>() {
+        @Override
+        public void onChanged(@Nullable FirestoreDataSource source) {
+
+        }
+    };
+
     private final Observer<LoadingState> mStateObserver =
             new Observer<LoadingState>() {
                 @Override
@@ -105,12 +113,25 @@ public abstract class FirestorePagingAdapter<T, VH extends RecyclerView.ViewHold
     }
 
     /**
+     * To attempt to refresh the list. It will reload the list from beginning.
+     */
+    public void refresh(){
+        FirestoreDataSource mFirebaseDataSource = mDataSource.getValue();
+        if (mFirebaseDataSource == null) {
+            Log.w(TAG, "Called refresh() when FirestoreDataSource is null!");
+            return;
+        }
+        mFirebaseDataSource.invalidate();
+    }
+
+    /**
      * Start listening to paging / scrolling events and populating adapter data.
      */
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     public void startListening() {
         mSnapshots.observeForever(mDataObserver);
         mLoadingState.observeForever(mStateObserver);
+        mDataSource.observeForever(mDataSourceObserver);
     }
 
     /**
@@ -121,6 +142,7 @@ public abstract class FirestorePagingAdapter<T, VH extends RecyclerView.ViewHold
     public void stopListening() {
         mSnapshots.removeObserver(mDataObserver);
         mLoadingState.removeObserver(mStateObserver);
+        mDataSource.removeObserver(mDataSourceObserver);
     }
 
     @Override
