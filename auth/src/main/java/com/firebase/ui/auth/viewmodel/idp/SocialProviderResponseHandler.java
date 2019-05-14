de7519b02ece0481054dbba9b022d174570c46a0
Felipe Gouveia <felipegouveia3@gmail.com>
2019-05-14 21:48:28 +0100

    Added verification for fail to sign in when account is disabled by th… (#1632)

--- auth/src/main/java/com/firebase/ui/auth/viewmodel/idp/SocialProviderResponseHandler.java
+++ auth/src/main/java/com/firebase/ui/auth/viewmodel/idp/SocialProviderResponseHandler.java
@@ -27,6 +27,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import java.util.List;
@@ -108,6 +109,16 @@ public class SocialProviderResponseHandler extends SignInViewModelBase {
                                                     e));
                                         }
                                     });
+                        } else if (e instanceof FirebaseAuthInvalidUserException){
+                            setResult(Resource.<IdpResponse>forFailure(
+                                    new FirebaseUiException(
+                                            ErrorCodes.ERROR_USER_DISABLED,
+                                            ErrorCodes.toFriendlyMessage(
+                                                    ErrorCodes.ERROR_USER_DISABLED
+                                            )
+                                    )
+                            ));
+                            return;
                         }
                     }
                 });
