ae1cf07937049bd5851cfeb0c167ca9755919822
Leonardo Siracusa <leosiracusa@google.com>
2019-11-19 10:31:44 -0800

    Add support for GitHub sign-in via Generic IDP

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,6 +1,6 @@
 object Config {
     const val version = "6.1.0"
-    val submodules = listOf("auth", "auth-github", "common", "firestore", "database", "storage")
+    val submodules = listOf("auth", "common", "firestore", "database", "storage")
 
     private const val kotlinVersion = "1.3.10"
 
