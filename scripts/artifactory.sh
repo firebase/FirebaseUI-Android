e38e0ec3549c6e7e7f0d4908dbf623d822ced5da
Sam Stern <samstern@google.com>
2020-03-23 12:27:58 -0400

    Build hang fix (#1757)

--- scripts/artifactory.sh
+++ scripts/artifactory.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-./gradlew :library:prepareArtifacts
-./gradlew :common:assembleRelease :common:prepareArtifacts :common:artifactoryPublish
-./gradlew :auth:assembleRelease :auth:prepareArtifacts :auth:artifactoryPublish
-./gradlew :database:assembleRelease :database:prepareArtifacts :database:artifactoryPublish
-./gradlew :firestore:assembleRelease :firestore:prepareArtifacts :firestore:artifactoryPublish
-./gradlew :storage:assembleRelease :storage:prepareArtifacts :storage:artifactoryPublish
+GRADLE_ARGS="--max-workers=2"
+
+./gradlew $GRADLE_ARGS :library:prepareArtifacts
+./gradlew $GRADLE_ARGS :common:assembleRelease :common:prepareArtifacts :common:artifactoryPublish
+./gradlew $GRADLE_ARGS :auth:assembleRelease :auth:prepareArtifacts :auth:artifactoryPublish
+./gradlew $GRADLE_ARGS :database:assembleRelease :database:prepareArtifacts :database:artifactoryPublish
+./gradlew $GRADLE_ARGS :firestore:assembleRelease :firestore:prepareArtifacts :firestore:artifactoryPublish
+./gradlew $GRADLE_ARGS :storage:assembleRelease :storage:prepareArtifacts :storage:artifactoryPublish
