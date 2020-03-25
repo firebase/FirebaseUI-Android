485453f5ecb55a64c39bde52ab8794f3d48383da
Sam <samstern@google.com>
2020-03-25 13:31:41 -0400

    Version 6.2.1

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-    const val version = "6.2.1-SNAPSHOT"
+    const val version = "6.2.1"
     val submodules = listOf("auth", "common", "firestore", "database", "storage")
 
     private const val kotlinVersion = "1.3.10"
