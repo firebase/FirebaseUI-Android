6507438789eb02b53088b4d86fcb9c824c8e8ea1
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-11-05 11:22:41 +0200

    refactor(authui): upgrade to Firebase Auth v20.0.0 (#1857)

--- auth/src/test/java/com/firebase/ui/auth/viewmodel/LinkingSocialProviderResponseHandlerTest.java
+++ auth/src/test/java/com/firebase/ui/auth/viewmodel/LinkingSocialProviderResponseHandlerTest.java
@@ -79,7 +79,7 @@ public class LinkingSocialProviderResponseHandlerTest {
         mHandler = new LinkingSocialProviderResponseHandler((Application) ApplicationProvider.getApplicationContext());
         FlowParameters testParams = TestHelper.getFlowParameters(Collections.singletonList(
                 GoogleAuthProvider.PROVIDER_ID));
-        mHandler.initializeForTesting(testParams, mMockAuth, null, null);
+        mHandler.initializeForTesting(testParams, mMockAuth, null);
     }
 
     @Test
@@ -279,7 +279,7 @@ public class LinkingSocialProviderResponseHandlerTest {
     private void setupAnonymousUpgrade() {
         FlowParameters testParams = TestHelper.getFlowParameters(Collections.singletonList(
                 GoogleAuthProvider.PROVIDER_ID), true);
-        mHandler.initializeForTesting(testParams, mMockAuth, null, null);
+        mHandler.initializeForTesting(testParams, mMockAuth, null);
         when(mMockAuth.getCurrentUser()).thenReturn(mMockUser);
         when(mMockUser.isAnonymous()).thenReturn(true);
         AuthOperationManager.getInstance().mScratchAuth = mScratchMockAuth;
