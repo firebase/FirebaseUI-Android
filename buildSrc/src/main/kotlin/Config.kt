ff9848210d76cc805f44470b56aa159b31044a5c
Sam <samstern@google.com>
2021-02-12 19:05:11 +0000

    Begin development on v7.2.0

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-    const val version = "7.1.1"
+    const val version = "7.2.0-SNAPSHOT"
     val submodules = listOf("auth", "common", "firestore", "database", "storage")
 
     private const val kotlinVersion = "1.3.72"
