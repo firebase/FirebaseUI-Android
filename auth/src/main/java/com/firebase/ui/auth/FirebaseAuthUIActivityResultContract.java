b22a385093899ca9298cf9883e9960d155c60faf
Jossi Wolf <jossi@snappmobile.io>
2021-03-03 21:33:38 +0100

    [#1916] Remove unused imports

--- auth/src/main/java/com/firebase/ui/auth/FirebaseAuthUIActivityResultContract.java
+++ auth/src/main/java/com/firebase/ui/auth/FirebaseAuthUIActivityResultContract.java
@@ -3,8 +3,6 @@ package com.firebase.ui.auth;
 import android.content.Context;
 import android.content.Intent;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 
 import androidx.activity.result.contract.ActivityResultContract;
