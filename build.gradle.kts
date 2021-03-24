0322f8562b1c29a6df07e147eb810acf10d990bd
Sam <samstern@google.com>
2021-03-24 12:02:12 +0000

    Get POM generation working

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,11 @@
 
 import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryExtension
-import com.jfrog.bintray.gradle.BintrayExtension
-import com.jfrog.bintray.gradle.tasks.RecordingCopyTask
-import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention
-import org.jfrog.gradle.plugin.artifactory.dsl.DoubleDelegateWrapper
-import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
-import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
 
 buildscript {
     repositories {
         google()
+        mavenCentral()
         jcenter()
         mavenLocal()
     }
@@ -20,7 +15,7 @@ buildscript {
         classpath(Config.Plugins.android)
         classpath(Config.Plugins.kotlin)
         classpath(Config.Plugins.google)
-        classpath(Config.Plugins.bintray)
+        classpath(Config.Plugins.mavenPublish)
         classpath(Config.Plugins.buildInfo)
     }
 }
@@ -64,21 +59,12 @@ val Project.reportsDir get() = "$buildDir/reports"
  */
 val Project.isLibrary get() = name == "library"
 
-/**
- * Returns the maven artifact name for a Project.
- */
-val Project.artifactName get() = if (isLibrary) "firebase-ui" else "firebase-ui-$name"
-
-/**
- * Returns the name for a Project's maven publication.
- */
-val Project.publicationName get() = if (isLibrary) "monolithLibrary" else "${name}Library"
-
 fun Project.configureAndroid() {
     if (name == "app" || name == "proguard-tests") {
         apply(plugin = "com.android.application")
     } else {
         apply(plugin = "com.android.library")
+        apply(plugin = "com.vanniktech.maven.publish")
     }
 
     configure<BaseExtension> {
@@ -168,243 +154,14 @@ fun Project.setupPublishing() {
 
             tasks.register("prepareArtifacts") {
                 dependsOn(javadocJar, sourcesJar, "assembleRelease")
-                dependsOn("generatePomFileForMonolithLibraryPublication")
                 dependsOn(*Config.submodules.map {
                     ":$it:prepareArtifacts"
                 }.toTypedArray())
             }
-
-            tasks.register("publishAllToMavenLocal") {
-                dependsOn("publishMonolithLibraryPublicationToMavenLocal")
-                dependsOn(*Config.submodules.map {
-                    ":$it:publish${it.capitalize()}LibraryPublicationToMavenLocal"
-                }.toTypedArray())
-            }
-
-            tasks.register("publishAllToCustomLocal") {
-                dependsOn("publishMonolithLibraryPublicationToCustomLocalRepository")
-                dependsOn(*Config.submodules.map {
-                    ":$it:publish${it.capitalize()}LibraryPublicationToCustomLocalRepository"
-                }.toTypedArray())
-            }
-
-            tasks.register("bintrayUploadAll") {
-                dependsOn("bintrayUpload")
-                dependsOn(*Config.submodules.map {
-                    ":$it:bintrayUpload"
-                }.toTypedArray())
-            }
         } else {
-            val pomTask = "generatePomFileFor${name.capitalize()}LibraryPublication"
             tasks.register("prepareArtifacts") {
-                dependsOn(javadocJar, sourcesJar, "assembleRelease", pomTask)
+                dependsOn("assembleRelease")
             }
         }
-
-        tasks.named("bintrayUpload").configure { dependsOn("prepareArtifacts") }
-    }
-
-    apply(plugin = "maven-publish")
-    apply(plugin = "com.jfrog.artifactory")
-    apply(plugin = "com.jfrog.bintray")
-
-    configure<PublishingExtension> {
-        repositories {
-            maven {
-                name = "CustomLocal"
-                // By passing -Pcustom_local=/some/path and running the
-                // publishLibraryPublicationToCustomLocalRepository task you can publish this library to a
-                // custom maven repository location on your machine.
-                url = uri(properties["custom_local"] ?: "/tmp/")
-            }
-
-            maven {
-                name = "BuildLocal"
-                url = uri("$buildDir/repo")
-            }
-        }
-
-        // We need to override the variables 'group' and 'version' on the 'Project' object in order
-        // to prevent the bintray plugin from creating 'unspecified' artifacts.
-        val groupName = "com.firebaseui"
-        group = groupName
-        version = Config.version
-
-        publications {
-            create<MavenPublication>(publicationName) {
-                groupId = groupName
-                artifactId = artifactName
-                version = Config.version
-
-                val releaseAar = "$buildDir/outputs/aar/${project.name}-release.aar"
-
-                logger.info("""
-                    |Creating maven publication '$publicationName'
-                    |    Group: $groupName
-                    |    Artifact: $artifactName
-                    |    Version: $version
-                    |    Aar: $releaseAar
-                """.trimMargin())
-
-                artifact(releaseAar)
-                artifact(javadocJar.get())
-                artifact(sourcesJar.get())
-
-                pom {
-                    name.set("FirebaseUI ${project.name.capitalize()}")
-                    description.set("Firebase UI for Android")
-                    url.set("https://github.com/firebase/FirebaseUI-Android")
-
-                    organization {
-                        name.set("Firebase")
-                        url.set("https://github.com/firebase")
-                    }
-
-                    scm {
-                        val scmUrl = "scm:git:git@github.com/firebase/firebaseui-android.git"
-                        connection.set(scmUrl)
-                        developerConnection.set(scmUrl)
-                        url.set(this@pom.url)
-                        tag.set("HEAD")
-                    }
-
-                    developers {
-                        developer {
-                            id.set("samtstern")
-                            name.set("Sam Stern")
-                            email.set("samstern@google.com")
-                            organization.set("Firebase")
-                            organizationUrl.set("https://firebase.google.com")
-                            roles.set(listOf("Project-Administrator", "Developer"))
-                            timezone.set("-8")
-                        }
-
-                        developer {
-                            id.set("SUPERCILEX")
-                            name.set("Alex Saveau")
-                            email.set("saveau.alexandre@gmail.com")
-                            roles.set(listOf("Developer"))
-                            timezone.set("-8")
-                        }
-                    }
-
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-
-                    withXml {
-                        asNode().appendNode("dependencies").apply {
-                            fun ResolvedDependency.write(scope: String) = appendNode("dependency").apply {
-                                val publicArtifactId = if (moduleGroup == groupName) {
-                                    "firebase-ui-$moduleName"
-                                } else {
-                                    moduleName
-                                }
-                                appendNode("groupId", moduleGroup)
-                                appendNode("artifactId", publicArtifactId)
-                                appendNode("version", moduleVersion)
-                                appendNode("scope", scope)
-                            }
-
-                            val apiDependencyNames = configurations["api"].dependencies.map {
-                                it.name
-                            }
-
-                            val implementationDependencyNames = configurations["implementation"].dependencies.map {
-                                it.name
-                            }
-
-                            configurations["releaseCompileClasspath"].resolvedConfiguration.firstLevelModuleDependencies.forEach {
-                                if (it.moduleName == "firebase-bom") {
-                                    return@forEach
-                                }
-
-                                if (apiDependencyNames.contains(it.moduleName)) {
-                                    it.write("compile")
-                                }
-
-                                if (implementationDependencyNames.contains(it.moduleName)) {
-                                    it.write("runtime")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    tasks.matching {
-        it.name.contains("publish") && it.name.contains("publication", true)
-    }.configureEach {
-        dependsOn("assembleRelease")
-    }
-
-    val bintrayUsername = properties["bintrayUser"] as String?
-            ?: System.getProperty("BINTRAY_USER") ?: System.getenv("BINTRAY_USER")
-    val bintrayKey = properties["bintrayKey"] as String?
-            ?: System.getProperty("BINTRAY_KEY") ?: System.getenv("BINTRAY_KEY")
-
-    configure<ArtifactoryPluginConvention> {
-        setContextUrl("https://oss.jfrog.org")
-        publish(closureOf<PublisherConfig> {
-            repository(closureOf<DoubleDelegateWrapper> {
-                invokeMethod("setRepoKey", "oss-snapshot-local")
-                invokeMethod("setUsername", bintrayUsername)
-                invokeMethod("setPassword", bintrayKey)
-            })
-        })
-    }
-
-    tasks.withType<ArtifactoryTask>().configureEach { publications(publicationName) }
-
-    configure<BintrayExtension> {
-        user = bintrayUsername
-        key = bintrayKey
-        setPublications(publicationName)
-
-        // When uploading, move and rename the generated POM
-        val pomSrc = "$buildDir/publications/$publicationName/pom-default.xml"
-        val pomDest = "com/firebaseui/$artifactName/${Config.version}/"
-        val pomName = "$artifactName-${Config.version}.pom"
-
-        val pubLog: (String) -> String = { name ->
-            val publishing = project.extensions
-                    .getByType(PublishingExtension::class.java)
-                    .publications[name] as MavenPublication
-            "'$name': ${publishing.artifacts}"
-        }
-        logger.info("""
-            |Bintray configuration for '$publicationName'
-            |    Artifact name: $artifactName
-            |    Artifacts: ${publications.joinToString(transform = pubLog)}
-        """.trimMargin())
-        logger.info("""
-            |POM transformation
-            |    Src: $pomSrc
-            |    Dest: $pomDest
-            |    Name: $pomName
-        """.trimMargin())
-
-        filesSpec(closureOf<RecordingCopyTask> {
-            from(pomSrc)
-            into(pomDest)
-            rename(KotlinClosure1<String, String>({ pomName }))
-        })
-
-        pkg(closureOf<BintrayExtension.PackageConfig> {
-            repo = "firebase-ui"
-            name = artifactName
-            userOrg = "firebaseui"
-            setLicenses("Apache-2.0")
-            vcsUrl = "https://github.com/firebase/FirebaseUI-Android.git"
-
-            version(closureOf<BintrayExtension.VersionConfig> {
-                name = Config.version
-            })
-        })
     }
 }
--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -15,7 +15,7 @@ object Config {
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
         const val google = "com.google.gms:google-services:4.3.3"
 
-        const val bintray = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5"
+        const val mavenPublish = "com.vanniktech:gradle-maven-publish-plugin:0.14.2"
         const val buildInfo = "org.jfrog.buildinfo:build-info-extractor-gradle:4.15.2"
     }
 
