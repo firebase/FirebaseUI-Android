6c4c3c586fc5761787fc03d43b59ad8a605a154f
rosariopf <rosariopf@google.com>
2022-06-28 14:37:04 +0100

    Begin development on 8.1.0

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-    const val version = "8.0.1"
+    const val version = "8.1.0-SNAPSHOT"
     val submodules = listOf("auth", "common", "firestore", "database", "storage")
 
     private const val kotlinVersion = "1.3.72"
