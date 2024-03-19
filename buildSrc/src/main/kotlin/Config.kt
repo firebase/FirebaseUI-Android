4c65e339510da5a6411db58b2388798c54fb72fe
rosariopf <rosariopf@google.com>
2024-03-19 20:08:07 +0000

    chore: compileSdk=34, minSdk=19

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -5,9 +5,9 @@ object Config {
     private const val kotlinVersion = "1.7.10"
 
     object SdkVersions {
-        const val compile = 29
-        const val target = 29
-        const val min = 16
+        const val compile = 34
+        const val target = 34
+        const val min = 19
     }
 
     object Plugins {
