b187ab5740fbbc713a1fc07d7f79293bea2709da
rosariopf <rosariopf@google.com>
2024-09-02 19:45:39 +0100

    chore: lower targetSDK temporarily
    Using 34 shows the following exception when trying email/password:
    
    ```
    java.lang.IllegalArgumentException: com.firebase.uidemo: Targeting U+ (version 34 and above) disallows creating or retrieving a PendingIntent with FLAG_MUTABLE, an implicit Intent within and without FLAG_NO_CREATE and FLAG_ALLOW_UNSAFE_IMPLICIT_INTENT for security reasons. To retrieve an already existing PendingIntent, use FLAG_NO_CREATE, however, to create a new PendingIntent with an implicit Intent use FLAG_IMMUTABLE.
    ```

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -5,8 +5,8 @@ object Config {
     private const val kotlinVersion = "1.7.10"
 
     object SdkVersions {
-        const val compile = 34
-        const val target = 34
+        const val compile = 33
+        const val target = 33
         const val min = 19
     }
 
