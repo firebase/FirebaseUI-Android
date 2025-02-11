d54c6f4825eb727aac1b6e9a372586c15d99b77b
rosariopf <rosariopf@google.com>
2025-02-11 02:16:14 +0000

    chore: upgrade Lint API

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -86,7 +86,7 @@ object Config {
         }
 
         object Lint {
-            private const val version = "26.5.0"
+            private const val version = "30.0.0"
 
             const val api = "com.android.tools.lint:lint-api:$version"
             const val tests = "com.android.tools.lint:lint-tests:$version"
