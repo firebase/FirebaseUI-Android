1e44a13a921f3a509e26df942fc93d82576a6f8f
rosariopf <rosariopf@google.com>
2025-03-19 17:02:06 +0000

    Begin development on 10.0.0

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-    const val version = "9.0.0"
+    const val version = "10.0.0-SNAPSHOT"
     val submodules = listOf("auth", "common", "firestore", "database", "storage")
 
     private const val kotlinVersion = "2.1.0"
