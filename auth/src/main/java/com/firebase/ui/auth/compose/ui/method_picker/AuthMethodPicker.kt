96ffa30021de2da3760c650cc68103b797d5c701
Ademola Fadumo <demolafadumo@gmail.com>
2025-09-30 12:59:56 +0100

    feat: TOS and PP footer, ui tests for AuthMethodPicker

--- auth/src/main/java/com/firebase/ui/auth/compose/ui/method_picker/AuthMethodPicker.kt
+++ auth/src/main/java/com/firebase/ui/auth/compose/ui/method_picker/AuthMethodPicker.kt
@@ -1,44 +1,26 @@
-package com.firebase.ui.auth.compose
+package com.firebase.ui.auth.compose.ui.method_picker
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.firebase.ui.auth.R
-import com.firebase.ui.auth.compose.AuthProviderButton
 import com.firebase.ui.auth.compose.configuration.AuthProvider
-import com.firebase.ui.auth.compose.configuration.stringprovider.DefaultAuthUIStringProvider
+import com.firebase.ui.auth.compose.configuration.string_provider.DefaultAuthUIStringProvider
 import com.firebase.ui.auth.compose.configuration.theme.AuthUIAsset
-
-//AuthMethodPicker(
-//   providers = listOf(GoogleAuthProvider(), EmailAuthProvider()),
-//   onProviderSelected = { provider -> /* ... */ }
-//)
+import com.firebase.ui.auth.compose.ui.components.AuthProviderButton
 
 /**
  * Renders the provider selection screen.
@@ -59,6 +41,8 @@ import com.firebase.ui.auth.compose.configuration.theme.AuthUIAsset
  * @param logo An optional logo to display.
  * @param onProviderSelected A callback when a provider is selected.
  * @param customLayout An optional custom layout composable for the provider buttons.
+ * @param termsOfServiceUrl The URL for the Terms of Service.
+ * @param privacyPolicyUrl The URL for the Privacy Policy.
  *
  * @since 10.0.0
  */
@@ -69,8 +53,11 @@ fun AuthMethodPicker(
     logo: AuthUIAsset? = null,
     onProviderSelected: (AuthProvider) -> Unit,
     customLayout: @Composable ((List<AuthProvider>, (AuthProvider) -> Unit) -> Unit)? = null,
+    termsOfServiceUrl: String? = null,
+    privacyPolicyUrl: String? = null,
 ) {
     val context = LocalContext.current
+    val inPreview = LocalInspectionMode.current
 
     Column(
         modifier = modifier
@@ -83,7 +70,8 @@ fun AuthMethodPicker(
                 modifier = Modifier
                     .weight(0.4f),
                 painter = it.painter,
-                contentDescription = "AuthMethodPicker logo",
+                contentDescription = if (inPreview) ""
+                else stringResource(R.string.fui_auth_method_picker_logo)
             )
         }
         if (customLayout != null) {
@@ -92,9 +80,9 @@ fun AuthMethodPicker(
             LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
-                    .weight(1f),
+                    .weight(1f)
+                    .testTag("AuthMethodPicker LazyColumn"),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                contentPadding = PaddingValues(bottom = 64.dp) // Space for text
             ) {
                 items(providers.size) { index ->
                     val provider = providers[index]
@@ -112,14 +100,18 @@ fun AuthMethodPicker(
                     }
                 }
             }
-            Text(
-                "By continuing, you are indicating that you accept our " +
-                        "Terms of Service and Privacy Policy.",
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(horizontal = 16.dp, vertical = 16.dp)
-            )
         }
+        AnnotatedStringResource(
+            context = context,
+            inPreview = inPreview,
+            previewText = "By continuing, you accept our Terms of Service and Privacy Policy.",
+            modifier = Modifier.padding(vertical = 16.dp),
+            id = R.string.fui_tos_and_pp,
+            links = arrayOf(
+                "Terms of Service" to (termsOfServiceUrl ?: ""),
+                "Privacy Policy" to (privacyPolicyUrl ?: "")
+            )
+        )
     }
 }
 
@@ -160,19 +152,12 @@ fun PreviewAuthMethodPicker() {
                 customParameters = emptyMap()
             ),
             AuthProvider.Anonymous,
-            AuthProvider.GenericOAuth(
-                providerId = "google.com",
-                scopes = emptyList(),
-                customParameters = emptyMap(),
-                buttonLabel = "Generic Provider",
-                buttonIcon = AuthUIAsset.Vector(Icons.Default.Star),
-                buttonColor = Color.Gray,
-                contentColor = Color.White
-            )
         ),
         logo = AuthUIAsset.Resource(R.drawable.fui_ic_check_circle_black_128dp),
         onProviderSelected = { provider ->
 
         },
+        termsOfServiceUrl = "https://example.com/terms",
+        privacyPolicyUrl = "https://example.com/privacy"
     )
 }
