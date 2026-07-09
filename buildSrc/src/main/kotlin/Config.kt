b97377019c51464d4a216a389cd974b177483718
Rosario Fernandes <rosariopf@google.com>
2026-07-09 11:52:54 +0200

    Version 10.0.0-beta03

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
-    const val version = "10.0.0-beta02"
+    const val version = "10.0.0-beta03"
     val submodules = listOf("auth", "common", "firestore", "database", "storage")
 
     object SdkVersions {
