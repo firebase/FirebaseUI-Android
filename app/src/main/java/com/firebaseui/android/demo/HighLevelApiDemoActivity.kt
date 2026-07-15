73765d95fb9e59444205f6f90389a04e1414dff8
demolaf <demolafadumo@gmail.com>
2026-07-15 15:26:40 +0100

    sample(app): use adaptive theme and Firebase brand color for demo top app bar

--- app/src/main/java/com/firebaseui/android/demo/HighLevelApiDemoActivity.kt
+++ app/src/main/java/com/firebaseui/android/demo/HighLevelApiDemoActivity.kt
@@ -71,18 +71,6 @@ class HighLevelApiDemoActivity : ComponentActivity() {
         val authUI = FirebaseAuthUI.getInstance()
         val emailLink = intent.getStringExtra(EmailLinkConstants.EXTRA_EMAIL_LINK)
 
-        val customTheme = AuthUITheme.Default.copy(
-            providerButtonShape = ShapeDefaults.ExtraLarge,
-            topAppBarColors = TopAppBarColors(
-                containerColor = Color(0xFF2E7D32),
-                scrolledContainerColor = Color(0xFF2E7D32),
-                navigationIconContentColor = Color.White,
-                titleContentColor = Color.White,
-                actionIconContentColor = Color.White,
-                subtitleContentColor = Color.White,
-            )
-        )
-
         class CustomAuthUIStringProvider(
             private val defaultProvider: AuthUIStringProvider
         ) : AuthUIStringProvider by defaultProvider {
@@ -94,124 +82,136 @@ class HighLevelApiDemoActivity : ComponentActivity() {
         val customStringProvider =
             CustomAuthUIStringProvider(DefaultAuthUIStringProvider(applicationContext))
 
-        val configuration = authUIConfiguration {
-            context = applicationContext
-            theme = customTheme
-            logo = AuthUIAsset.Resource(R.drawable.firebase_auth)
-            tosUrl = "https://policies.google.com/terms"
-            privacyPolicyUrl = "https://policies.google.com/privacy"
-            isAnonymousUpgradeEnabled = false
-            isMfaEnabled = false
-            stringProvider = customStringProvider
-            transitions = AuthUITransitions(
-                enterTransition = { slideInHorizontally { it } },
-                exitTransition = { slideOutHorizontally { -it } },
-                popEnterTransition = { slideInHorizontally { -it } },
-                popExitTransition = { slideOutHorizontally { it } }
-            )
-            providers {
-                provider(AuthProvider.Anonymous)
-                provider(
-                    AuthProvider.Google(
-                        scopes = listOf("email"),
-                        serverClientId = "406099696497-a12gakvts4epfk5pkio7dphc1anjiggc.apps.googleusercontent.com",
-                    )
-                )
-                provider(
-                    AuthProvider.Email(
-                        isDisplayNameRequired = true,
-                        isEmailLinkForceSameDeviceEnabled = false,
-                        isEmailLinkSignInEnabled = true,
-                        emailLinkActionCodeSettings = actionCodeSettings {
-                            url = "https://flutterfire-e2e-tests.firebaseapp.com"
-                            handleCodeInApp = true
-                            setAndroidPackageName(
-                                "com.firebaseui.android.demo",
-                                true,
-                                null
-                            )
-                        },
-                        isNewAccountsAllowed = true,
-                        minimumPasswordLength = 8,
-                        passwordValidationRules = listOf(
-                            PasswordRule.MinimumLength(8),
-                            PasswordRule.RequireLowercase,
-                            PasswordRule.RequireUppercase,
-                        ),
-                    )
-                )
-                provider(
-                    AuthProvider.Phone(
-                        defaultNumber = null,
-                        defaultCountryCode = null,
-                        allowedCountries = emptyList(),
-                        smsCodeLength = 6,
-                        timeout = 120L,
-                        isInstantVerificationEnabled = true
-                    )
-                )
-                provider(
-                    AuthProvider.Facebook()
-                )
-                provider(
-                    AuthProvider.Twitter(
-                        customParameters = emptyMap()
-                    )
-                )
-                provider(
-                    AuthProvider.Apple(
-                        customParameters = emptyMap(),
-                        locale = null
-                    )
-                )
-                provider(
-                    AuthProvider.Microsoft(
-                        scopes = emptyList(),
-                        tenant = "",
-                        customParameters = emptyMap(),
-                    )
-                )
-                provider(
-                    AuthProvider.Github(
-                        scopes = emptyList(),
-                        customParameters = emptyMap(),
-                    )
-                )
-                provider(
-                    AuthProvider.Yahoo(
-                        scopes = emptyList(),
-                        customParameters = emptyMap(),
-                    )
-                )
-                provider(
-                    AuthProvider.GenericOAuth(
-                        providerName = "LINE",
-                        providerId = "oidc.line",
-                        scopes = emptyList(),
-                        customParameters = emptyMap(),
-                        buttonLabel = "Sign in with LINE",
-                        buttonIcon = AuthUIAsset.Resource(R.drawable.ic_line_logo_24dp),
-                        buttonColor = Color(0xFF06C755),
-                        contentColor = Color.White
-                    )
-                )
-                provider(
-                    AuthProvider.GenericOAuth(
-                        providerName = "Discord",
-                        providerId = "oidc.discord",
-                        scopes = emptyList(),
-                        customParameters = emptyMap(),
-                        buttonLabel = "Sign in with Discord",
-                        buttonIcon = AuthUIAsset.Resource(R.drawable.ic_discord_24dp),
-                        buttonColor = Color(0xFF5865F2),
-                        contentColor = Color.White
-                    )
-                )
-            }
-        }
-
         setContent {
-            AuthUITheme {
+            val customTheme = AuthUITheme.Adaptive.copy(
+                providerButtonShape = ShapeDefaults.ExtraLarge,
+                topAppBarColors = TopAppBarColors(
+                    containerColor = Color(0xFFFFA000),
+                    scrolledContainerColor = Color(0xFFFFA000),
+                    navigationIconContentColor = Color.Black,
+                    titleContentColor = Color.Black,
+                    actionIconContentColor = Color.Black,
+                    subtitleContentColor = Color.Black,
+                )
+            )
+
+            val configuration = authUIConfiguration {
+                context = applicationContext
+                theme = customTheme
+                logo = AuthUIAsset.Resource(R.drawable.firebase_auth)
+                tosUrl = "https://policies.google.com/terms"
+                privacyPolicyUrl = "https://policies.google.com/privacy"
+                isAnonymousUpgradeEnabled = false
+                isMfaEnabled = false
+                stringProvider = customStringProvider
+                transitions = AuthUITransitions(
+                    enterTransition = { slideInHorizontally { it } },
+                    exitTransition = { slideOutHorizontally { -it } },
+                    popEnterTransition = { slideInHorizontally { -it } },
+                    popExitTransition = { slideOutHorizontally { it } }
+                )
+                providers {
+                    provider(AuthProvider.Anonymous)
+                    provider(
+                        AuthProvider.Google(
+                            scopes = listOf("email"),
+                            serverClientId = "406099696497-a12gakvts4epfk5pkio7dphc1anjiggc.apps.googleusercontent.com",
+                        )
+                    )
+                    provider(
+                        AuthProvider.Email(
+                            isDisplayNameRequired = true,
+                            isEmailLinkForceSameDeviceEnabled = false,
+                            isEmailLinkSignInEnabled = true,
+                            emailLinkActionCodeSettings = actionCodeSettings {
+                                url = "https://flutterfire-e2e-tests.firebaseapp.com"
+                                handleCodeInApp = true
+                                setAndroidPackageName(
+                                    "com.firebaseui.android.demo",
+                                    true,
+                                    null
+                                )
+                            },
+                            isNewAccountsAllowed = true,
+                            minimumPasswordLength = 8,
+                            passwordValidationRules = listOf(
+                                PasswordRule.MinimumLength(8),
+                                PasswordRule.RequireLowercase,
+                                PasswordRule.RequireUppercase,
+                            ),
+                        )
+                    )
+                    provider(
+                        AuthProvider.Phone(
+                            defaultNumber = null,
+                            defaultCountryCode = null,
+                            allowedCountries = emptyList(),
+                            smsCodeLength = 6,
+                            timeout = 120L,
+                            isInstantVerificationEnabled = true
+                        )
+                    )
+                    provider(
+                        AuthProvider.Facebook()
+                    )
+                    provider(
+                        AuthProvider.Twitter(
+                            customParameters = emptyMap()
+                        )
+                    )
+                    provider(
+                        AuthProvider.Apple(
+                            customParameters = emptyMap(),
+                            locale = null
+                        )
+                    )
+                    provider(
+                        AuthProvider.Microsoft(
+                            scopes = emptyList(),
+                            tenant = "",
+                            customParameters = emptyMap(),
+                        )
+                    )
+                    provider(
+                        AuthProvider.Github(
+                            scopes = emptyList(),
+                            customParameters = emptyMap(),
+                        )
+                    )
+                    provider(
+                        AuthProvider.Yahoo(
+                            scopes = emptyList(),
+                            customParameters = emptyMap(),
+                        )
+                    )
+                    provider(
+                        AuthProvider.GenericOAuth(
+                            providerName = "LINE",
+                            providerId = "oidc.line",
+                            scopes = emptyList(),
+                            customParameters = emptyMap(),
+                            buttonLabel = "Sign in with LINE",
+                            buttonIcon = AuthUIAsset.Resource(R.drawable.ic_line_logo_24dp),
+                            buttonColor = Color(0xFF06C755),
+                            contentColor = Color.White
+                        )
+                    )
+                    provider(
+                        AuthProvider.GenericOAuth(
+                            providerName = "Discord",
+                            providerId = "oidc.discord",
+                            scopes = emptyList(),
+                            customParameters = emptyMap(),
+                            buttonLabel = "Sign in with Discord",
+                            buttonIcon = AuthUIAsset.Resource(R.drawable.ic_discord_24dp),
+                            buttonColor = Color(0xFF5865F2),
+                            contentColor = Color.White
+                        )
+                    )
+                }
+            }
+
+            AuthUITheme(theme = customTheme) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
