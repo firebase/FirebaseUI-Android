f2d562d32474977fc93bad457715a20c47a73125
Ademola Fadumo <48495111+demolaf@users.noreply.github.com>
2026-09-21 09:45:27 +0100

    ci: bring :app and :e2eTest under the Android Lint gate (#2503)

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -23,20 +23,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    lint {
-        // Common lint options across all modules
-        disable += mutableSetOf(
-            "IconExpectedSize",
-            "InvalidPackage", // Firestore uses GRPC which makes lint mad
-            "NewerVersionAvailable", "GradleDependency", // For reproducible builds
-            "SelectableText", "SyntheticAccessor" // We almost never care about this
-        )
-
-        checkAllWarnings = true
-        warningsAsErrors = true
-        abortOnError = true
-
-    }
 
     buildTypes {
         named("release").configure {
